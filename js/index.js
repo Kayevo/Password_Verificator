1c7f408055883f1fc3ca235901bd6adda388a67a
Kayevo <kayevo@protonmail.com>
2022-01-02 21:12:12 -0300

    The ASCII characters base now is larger

--- js/index.js
+++ js/index.js
@@ -1,8 +1,23 @@
 class PasswordGenerator {
   constructor() {}
 
-  generatePassword() {
-    alert("Password generated.");
+  
+  generateRandomPassword(passwordLength) {
+
+    var randomPassword = "";
+    var alphanumericBaseString =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefABCDEF01234567!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+    var alphaBaseStringLength = alphanumericBaseString.length;
+
+    for (var i = 0; i < passwordLength; i++) {
+      randomPassword =
+        randomPassword +
+        alphanumericBaseString.charAt(
+          Math.floor(Math.random() * alphaBaseStringLength)
+        );
+    }
+
+    alert("Password generated: "+randomPassword);
   }
 }
 
